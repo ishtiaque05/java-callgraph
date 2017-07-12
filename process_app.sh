b8add068ab7360ae6d322cfa44c6b01a5af53548
Rodrigo Bruno <rbruno@gsd.inesc-id.pt>
2017-07-12 21:22:05 +0100

    Updated scripts. Almost ready.

--- process_app.sh
+++ process_app.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
-target=$1
-# TODO - remove [I] calls
-java -classpath target/javacg-0.1-SNAPSHOT-static.jar gr.gousiosg.javacg.stat.JCallGraph  $target | grep "^M:" | grep -v "\[I\]" | sed 's/M://g' | sort | uniq  | ./process_trace.py
+
+basedir=$(dirname "$0")
+output_folder=$basedir
+main="gr.gousiosg.javacg.stat.JCallGraph"
+
+# Test purposes.
+#target="target/classes/gr/gousiosg/javacg/stat/test/polymorphism"
+#classpath="$classpath $basedir/target/javacg-0.1-SNAPSHOT-static.jar"
+
+# Test 2
+classpath="$classpath $basedir/target/javacg-0.1-SNAPSHOT-static.jar"
+target=$classpath
+
+echo "Launching java..."
+java -cp $classpath $main $target &> $output_folder/app.log 
+echo "Launching java...Done"
+
+echo "Launching python..."
+#cat $output_folder/app.log | grep "^M:" | grep -v "\[I\]" | sed 's/M://g' | sort | uniq  | $basedir/process_trace.py $output_folder
+echo "Launching python...Done"
--- process_trace.py
+++ process_trace.py
@@ -5,68 +5,98 @@ import sys
 # Dictionary of caller -> callee
 # caller := <class>:<method>(<args>):<bci>
 # calle := <class>:<method>(<args>):[<bci>]
-callgraph = {}
+callmap = {}
 
-# This method will search and return a list of callees that are called by the
-# caller node. Note that this caller node does not contain a bci.
-def findcallers(callee):
-  result = []
-  for caller in callgraph:
-    if callee == caller.split("#")[0]:
-      result.append(caller)
-  return result
+# Dictionary of callee -> caller
+# caller := <class>:<method>(<args>):<bci>
+# calle := <class>:<method>(<args>):[<bci>]
+rcallmap = {}
 
-def calltree (caller, depth):
-  if caller not in callgraph:
+def callgraph(graph, output):
+  for node in graph:
+    print(node, file=output)
+    callbranch(graph, node, 1, output)
+
+def callbranch(graph, caller, depth, output):
+  if caller not in graph:
     return
-  for callee in callgraph[caller]:
+  for callee in graph[caller]:
     for i in range(0, depth):
-      print("\t", end="")
-    print(callee)
-    calltree(callee, depth + 1)
+      print("\t", end="", file=output)
+    print(callee, file=output)
+    callbranch(graph, callee, depth + 1, output)
 
-def buildcallgraph(caller, callee):
-  if caller not in callgraph:
-    callgraph[caller] = []
+def buildcallmap(caller, callee):
+  if caller not in callmap:
+    callmap[caller] = []
 
-  if callee not in callgraph[caller]:
-    callgraph[caller].append(callee)
+  if callee not in callmap[caller]:
+    callmap[caller].append(callee)
 
-def finishcallgraph():
+def buildrcallmap():
+  for caller in callmap:
+    for callee in callmap[caller]:
+      if callee not in rcallmap:
+        rcallmap[callee] = []
+      if caller not in rcallmap[callee]:
+        rcallmap[callee].append(caller)
+
+def finishcallmap():
+  # This method will search and return a list of callees that are called by the
+  # caller node. Note that this caller node does not contain a bci.
+  def findcallers(callee):
+    result = []
+    for caller in callmap:
+      if callee == caller.split("#")[0]:
+        result.append(caller)
+    return result
+
   # Up to this point, callees do not have a BCI. This cycle will add BCIs to
   # callees
-  for caller in callgraph:
+  for caller in callmap:
     ncallees = []
-    for callee in callgraph[caller]:
+    for callee in callmap[caller]:
       tmp = findcallers(callee)
       if not tmp:
         ncallees.append(callee)
       else:
         ncallees.extend(tmp)
-    callgraph[caller] = ncallees
+    callmap[caller] = ncallees
 
-def printgraph(graph):
-  # Printing callgraph
+def printgraph(graph, output):
   for caller in graph:
-    print(caller)
+    print(caller, file=output)
     for callee in graph[caller]:
-      print("\t" + callee)
+      print("\t" + callee, file=output)
 
 for line in sys.stdin:
   splits = line.strip().split(" ")
   caller = splits[0]
   callee = splits[2]
-  buildcallgraph(caller, callee)
+  buildcallmap(caller, callee)
 
-finishcallgraph()
-printgraph(callgraph)
+finishcallmap()
+buildrcallmap()
+
+print(sys.getrecursionlimit())
+sys.setrecursionlimit(100000)
+
+print("Printing callmap")
+output_file = open(sys.argv[1] + "/app.callmap", "w")
+printgraph(callmap, output_file)
+output_file.close()
 
 print("Going for call tree")
-#string="gr.gousiosg.javacg.stat.test.polymorphism.Test:main(java.lang.String[])#29"
-#print(string)
-#calltree(string, 1)
+output_file = open(sys.argv[1] + "/app.callgraph", "w")
+callgraph(callmap, output_file)
+output_file.close()
 
-for caller in callgraph:
-  print(caller)
-  calltree(caller, 1)
+print("Printing rcallmap")
+output_file = open(sys.argv[1] + "/app.rcallmap", "w")
+printgraph(rcallmap, output_file)
+output_file.close()
 
+print("Going for call tree")
+output_file = open(sys.argv[1] + "/app.rcallgraph", "w")
+callgraph(rcallmap, output_file)
+output_file.close()
