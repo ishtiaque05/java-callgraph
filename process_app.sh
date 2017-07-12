56ec970de7c4f84e20554c2e6dfa2f972a71ab6d
Rodrigo Bruno <rbruno@gsd.inesc-id.pt>
2017-07-12 12:28:28 +0100

    Scripts now are able to generate stack traces.

--- process_app.sh
+++ process_app.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 target=$1
-java -classpath target/javacg-0.1-SNAPSHOT-static.jar gr.gousiosg.javacg.stat.JCallGraph  $target #| grep "^M:" | sed 's/M://g' | sort | uniq | ./process_trace.py
+# TODO - remove [I] calls
+java -classpath target/javacg-0.1-SNAPSHOT-static.jar gr.gousiosg.javacg.stat.JCallGraph  $target | grep "^M:" | grep -v "\[I\]" | sed 's/M://g' | sort | uniq  | ./process_trace.py
--- process_trace.py
+++ process_trace.py
@@ -3,19 +3,70 @@
 import sys
 
 # Dictionary of caller -> callee
+# caller := <class>:<method>(<args>):<bci>
+# calle := <class>:<method>(<args>):[<bci>]
 callgraph = {}
+
+# This method will search and return a list of callees that are called by the
+# caller node. Note that this caller node does not contain a bci.
+def findcallers(callee):
+  result = []
+  for caller in callgraph:
+    if callee == caller.split("#")[0]:
+      result.append(caller)
+  return result
+
+def calltree (caller, depth):
+  if caller not in callgraph:
+    return
+  for callee in callgraph[caller]:
+    for i in range(0, depth):
+      print("\t", end="")
+    print(callee)
+    calltree(callee, depth + 1)
+
+def buildcallgraph(caller, callee):
+  if caller not in callgraph:
+    callgraph[caller] = []
+
+  if callee not in callgraph[caller]:
+    callgraph[caller].append(callee)
+
+def finishcallgraph():
+  # Up to this point, callees do not have a BCI. This cycle will add BCIs to
+  # callees
+  for caller in callgraph:
+    ncallees = []
+    for callee in callgraph[caller]:
+      tmp = findcallers(callee)
+      if not tmp:
+        ncallees.append(callee)
+      else:
+        ncallees.extend(tmp)
+    callgraph[caller] = ncallees
+
+def printgraph(graph):
+  # Printing callgraph
+  for caller in graph:
+    print(caller)
+    for callee in graph[caller]:
+      print("\t" + callee)
 
 for line in sys.stdin:
   splits = line.strip().split(" ")
   caller = splits[0]
-  callee = splits[1]
+  callee = splits[2]
+  buildcallgraph(caller, callee)
 
-  if not caller in callgraph:
-    callgraph[caller] = [callee]
-  else:
-    callgraph[caller] = callgraph[caller].append(callee)
+finishcallgraph()
+printgraph(callgraph)
 
-for caller, calleelst in callgraph.items():
+print("Going for call tree")
+#string="gr.gousiosg.javacg.stat.test.polymorphism.Test:main(java.lang.String[])#29"
+#print(string)
+#calltree(string, 1)
+
+for caller in callgraph:
   print(caller)
-  for callee in calleelst:
-    print("\t" + callee)
+  calltree(caller, 1)
+
