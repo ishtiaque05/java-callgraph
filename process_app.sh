b6a8680c18ed1d720890d52ac92cda170389dfda
rodrigo-bruno <rbruno@gsd.inesc-id.pt>
2017-07-14 08:29:56 +0100

    Updates to script files. Still under development.

--- process_app.sh
+++ process_app.sh
@@ -7,15 +7,36 @@ main="gr.gousiosg.javacg.stat.JCallGraph"
 # Test purposes.
 #target="target/classes/gr/gousiosg/javacg/stat/test/polymorphism"
 #classpath="$classpath $basedir/target/javacg-0.1-SNAPSHOT-static.jar"
+package="gr.gousiosg.javacg.stat.test.polymorphism"
+package="gr.gousiosg.javacg.stat"
+
 
 # Test 2
 classpath="$classpath $basedir/target/javacg-0.1-SNAPSHOT-static.jar"
 target=$classpath
 
+
+function run_python {
+  echo "Launching python..."
+  cat $output_folder/app.log | grep "^M:" | grep -v "\[I\]" | sed 's/M://g' | sort | uniq  | $basedir/process_trace.py $output_folder $package
+  echo "Launching python...Done"
+}
+
 echo "Launching java..."
 java -cp $classpath $main $target &> $output_folder/app.log 
 echo "Launching java...Done"
 
-echo "Launching python..."
-#cat $output_folder/app.log | grep "^M:" | grep -v "\[I\]" | sed 's/M://g' | sort | uniq  | $basedir/process_trace.py $output_folder
-echo "Launching python...Done"
+cat app.log | grep "^N" | sed 's/NA://g' | sed 's/N://g' |  awk -F'#' '{print $1}' | sort | uniq &> $output_folder/app.allocs
+echo "Number of allocation methods = `cat $output_folder/app.allocs | wc -l`"
+
+#while true; do
+#  read -p "Run python tool? " run
+#  case $run in
+#    [Yy]* ) run_python; break;;
+#    [Nn]* ) break;;
+#        * ) echo "Please answer run or load. ";;
+#  esac
+#done
+run_python
+
+beep
--- process_trace.py
+++ process_trace.py
@@ -12,19 +12,58 @@ callmap = {}
 # calle := <class>:<method>(<args>):[<bci>]
 rcallmap = {}
 
-def callgraph(graph, output):
-  for node in graph:
-    print(node, file=output)
-    callbranch(graph, node, 1, output)
+# Target is <class>:<method>(<args>)
+def reverseTraversal(result, stack, callee, output, depth):
+  callers = [] if callee not in rcallmap else rcallmap[callee]
+  stripped_callers = []
+  if callee not in result:
+    result[callee] = []
+  for caller in callers:
+    if caller in stack:
+      continue
+    print("d=" + str(depth) + "\t", file=output, end="")
+    for i in range(0, depth): print(" ", end="", file=output)
+    print(caller, file=output)
+    if caller not in result[callee]:
+      result[callee].append(caller)
+    stack.append(caller)
+    striped = caller.split("#")[0]
+    if striped not in stripped_callers:
+      stripped_callers.append(striped)
+      reverseTraversal(result, stack, striped, output, depth + 1)
+    stack.pop()
+  return result
 
-def callbranch(graph, caller, depth, output):
-  if caller not in graph:
-    return
-  for callee in graph[caller]:
-    for i in range(0, depth):
-      print("\t", end="", file=output)
-    print(callee, file=output)
-    callbranch(graph, callee, depth + 1, output)
+# Target is <class>:<method>(<args>). Reult is list of <class>:<method>(<args>)#<bci>
+def findCallers(imap, target):
+  result = []
+  for caller in imap:
+    if caller.startswith(target) and caller not in result:
+      result.append(caller)
+  return result
+
+def findMultiCallers(imap, targets):
+  result = []
+  for target in targets:
+    result.extend(findCallers(imap, target))
+  return result
+
+# Target is <class>:<method>#<bci>
+# Imap is forward call map
+def findDivergence(imap, stack, target, output):
+  values = [] if target not in imap else imap[target]
+  if values:
+    callers = findMultiCallers(imap, values)
+    if len(callers) > 1:
+      print("important= " + str(callers))
+    for caller in callers:
+      if caller in stack:
+        continue
+      else:
+        stack.append(caller)
+        findDivergence(imap, stack, caller, output)
+        stack.pop() 
+
 
 def buildcallmap(caller, callee):
   if caller not in callmap:
@@ -33,37 +72,33 @@ def buildcallmap(caller, callee):
   if callee not in callmap[caller]:
     callmap[caller].append(callee)
 
-def buildrcallmap():
-  for caller in callmap:
-    for callee in callmap[caller]:
-      if callee not in rcallmap:
-        rcallmap[callee] = []
-      if caller not in rcallmap[callee]:
-        rcallmap[callee].append(caller)
+# Find keys that are never values
+# Note this is prepared to work with a forward call map
+# in which keys have bci and values don't
+def entrypoints(imap):
+  result = []
+  for k1 in imap:
+    sk1 = k1.split("#")[0]
+    found = False
+    for k2 in imap:
+      if sk1 in imap[k2]:
+        found = True
+        break
+    if not found:
+      result.append(k1)
+  return list(set(result))
 
-def finishcallmap():
-  # This method will search and return a list of callees that are called by the
-  # caller node. Note that this caller node does not contain a bci.
-  def findcallers(callee):
-    result = []
-    for caller in callmap:
-      if callee == caller.split("#")[0]:
-        result.append(caller)
-    return result
+def swapmap(imap):
+  result = {}
+  for caller in imap:
+    for callee in imap[caller]:
+      if callee not in result:
+        result[callee] = []
+      if caller not in result[callee]:
+        result[callee].append(caller)
+  return result
 
-  # Up to this point, callees do not have a BCI. This cycle will add BCIs to
-  # callees
-  for caller in callmap:
-    ncallees = []
-    for callee in callmap[caller]:
-      tmp = findcallers(callee)
-      if not tmp:
-        ncallees.append(callee)
-      else:
-        ncallees.extend(tmp)
-    callmap[caller] = ncallees
-
-def printgraph(graph, output):
+def printmap(graph, output):
   for caller in graph:
     print(caller, file=output)
     for callee in graph[caller]:
@@ -75,28 +110,31 @@ for line in sys.stdin:
   callee = splits[2]
   buildcallmap(caller, callee)
 
-finishcallmap()
-buildrcallmap()
+rcallmap = swapmap(callmap)
 
-print(sys.getrecursionlimit())
-sys.setrecursionlimit(100000)
+print("callmap with " + str(len(callmap)) + " keys")
+print("rcallmap with " + str(len(rcallmap)) + " keys")
 
 print("Printing callmap")
 output_file = open(sys.argv[1] + "/app.callmap", "w")
-printgraph(callmap, output_file)
-output_file.close()
-
-print("Going for call tree")
-output_file = open(sys.argv[1] + "/app.callgraph", "w")
-callgraph(callmap, output_file)
+printmap(callmap, output_file)
 output_file.close()
 
 print("Printing rcallmap")
 output_file = open(sys.argv[1] + "/app.rcallmap", "w")
-printgraph(rcallmap, output_file)
+printmap(rcallmap, output_file)
 output_file.close()
 
-print("Going for call tree")
-output_file = open(sys.argv[1] + "/app.rcallgraph", "w")
-callgraph(rcallmap, output_file)
-output_file.close()
+# Look in rcallgraph for any key
+#target="gr.gousiosg.javacg.stat.MethodVisitor:prepareCall(org.apache.bcel.classfile.JavaClass,org.apache.bcel.generic.ReferenceType,org.apache.bcel.generic.InvokeInstruction)"
+target="gr.gousiosg.javacg.stat.ClassVisitor:<init>(org.apache.bcel.classfile.JavaClass)"
+print("Looking into " + target)
+revers = reverseTraversal({}, [], target, sys.stdout, 1)
+print("Printing reversed map")
+printmap(revers, sys.stdout)
+print("Printing swaped reversed map")
+allocmap = swapmap(revers)
+printmap(allocmap, sys.stdout)
+print(entrypoints(allocmap))
+for entrypoint in entrypoints(allocmap):
+  findDivergence(allocmap, [], entrypoint, sys.stdout)
